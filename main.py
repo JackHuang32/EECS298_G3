6d7f702015567d4d6f6d1f53c6a8cf6986cd9381
JackHuang32 <jack900707@gmail.com>
2025-03-09 14:15:24 -0700

    define new pooling networks for experiments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import torch
 from torch_geometric.datasets import TUDataset
 from torch_geometric.data import DataLoader
 from torch_geometric import utils
-from networks import  Net
+from networks import SAGNet, TopKNet, EdgeNet, ClusterNet, ASANet
 import torch.nn.functional as F
 import argparse
 import os
@@ -31,7 +31,8 @@ parser.add_argument('--patience', type=int, default=50,
                     help='patience for earlystopping')
 parser.add_argument('--pooling_layer_type', type=str, default='GCNConv',
                     help='DD/PROTEINS/NCI1/NCI109/Mutagenicity')
-
+parser.add_argument('--pooling_method', type=str, default='SAGPooling',
+                    help='SAGPooling/TopKPooling')
 args = parser.parse_args()
 args.device = 'cpu'
 torch.manual_seed(args.seed)
@@ -47,12 +48,24 @@ num_val = int(len(dataset)*0.1)
 num_test = len(dataset) - (num_training+num_val)
 training_set,validation_set,test_set = random_split(dataset,[num_training,num_val,num_test])
 
-
+def get_model(args):
+    if args.pooling_method == 'SAGPooling':
+        return SAGNet
+    elif args.pooling_method == 'TopKPooling':
+        return TopKNet
+    elif args.pooling_method == 'EdgePooling':
+        return EdgeNet
+    elif args.pooling_method == 'ClusterPooling':
+        return ClusterNet
+    elif args.pooling_method == 'ASAPooling':
+        return ASANet
+    else:
+        raise ValueError("Invalid pooling layer type")
 
 train_loader = DataLoader(training_set, batch_size=args.batch_size, shuffle=True)
-val_loader = DataLoader(validation_set,batch_size=args.batch_size,shuffle=False)
+val_loader = DataLoader(validation_set, batch_size=args.batch_size,shuffle=False)
 test_loader = DataLoader(test_set,batch_size=1,shuffle=False)
-model = Net(args).to(args.device)
+model = get_model(args)(args).to(args.device)
 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
 
@@ -71,6 +84,20 @@ def test(model,loader):
 
 min_loss = 1e10
 patience = 0
+import matplotlib.pyplot as plt
+
+train_losses = []
+train_accs = []
+val_losses = []
+val_accs = []
+
+plt.ion()
+fig, ax = plt.subplots()
+fig2, ax2 = plt.subplots()
+ax.set_xlabel('Epoch')
+ax.set_ylabel('Loss')
+ax2.set_xlabel('Epoch')
+ax2.set_ylabel('Acc')
 
 for epoch in range(args.epochs):
     model.train()
@@ -82,7 +109,8 @@ for epoch in range(args.epochs):
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-    val_acc,val_loss = test(model,val_loader)
+    val_acc, val_loss = test(model,val_loader)
+    train_acc, train_loss = test(model,train_loader)
     print("Validation loss:{}\taccuracy:{}".format(val_loss,val_acc))
     if val_loss < min_loss:
         torch.save(model.state_dict(),'latest.pth')
@@ -92,9 +120,26 @@ for epoch in range(args.epochs):
     else:
         patience += 1
     if patience > args.patience:
-        break 
+        break
+    train_losses.append(train_loss)
+    val_losses.append(val_loss)
+    train_accs.append(train_acc)
+    val_accs.append(val_acc)
+     
+train_loss_line, = ax.plot(train_losses, label='Train Loss')
+val_loss_line, = ax.plot(val_losses, label='Validation Loss')
+train_acc_line, = ax2.plot(train_accs, label='Train Accuracy')
+val_acc_line, = ax2.plot(val_accs, label='Validation Accuracy')
 
-model = Net(args).to(args.device)
+ax.legend()
+ax2.legend()
+
+ax.set_title(f'{args.pooling_method} Training and Validation Loss')
+ax2.set_title(f'{args.pooling_method} Training and Validation Accuracy')
+fig.savefig(f'{args.pooling_method}_loss.png')
+fig2.savefig(f'{args.pooling_method}_acc.png')
+
+model = model.to(args.device)
 model.load_state_dict(torch.load('latest.pth'))
 test_acc,test_loss = test(model,test_loader)
 print("Test accuarcy:{}".format(test_acc))
--- networks.py
+++ networks.py
@@ -1,6 +1,7 @@
 import torch
 from torch_geometric.nn import GCNConv
 from torch_geometric.nn import GraphConv, TopKPooling
+from torch_geometric.nn.pool import SAGPooling, EdgePooling, ClusterPooling, ASAPooling
 from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
 import torch.nn.functional as F
 
@@ -8,9 +9,9 @@ import torch.nn.functional as F
 
 
 
-class Net(torch.nn.Module):
+class SAGNet(torch.nn.Module):
     def __init__(self,args):
-        super(Net, self).__init__()
+        super(SAGNet, self).__init__()
         self.args = args
         self.num_features = args.num_features
         self.nhid = args.nhid
@@ -19,11 +20,11 @@ class Net(torch.nn.Module):
         self.dropout_ratio = args.dropout_ratio
         
         self.conv1 = GCNConv(self.num_features, self.nhid)
-        self.pool1 = TopKPooling(self.nhid, ratio=self.pooling_ratio)
+        self.pool1 = SAGPooling(self.nhid, ratio=self.pooling_ratio)
         self.conv2 = GCNConv(self.nhid, self.nhid)
-        self.pool2 = TopKPooling(self.nhid, ratio=self.pooling_ratio)
+        self.pool2 = SAGPooling(self.nhid, ratio=self.pooling_ratio)
         self.conv3 = GCNConv(self.nhid, self.nhid)
-        self.pool3 = TopKPooling(self.nhid, ratio=self.pooling_ratio)
+        self.pool3 = SAGPooling(self.nhid, ratio=self.pooling_ratio)
 
         self.lin1 = torch.nn.Linear(self.nhid*2, self.nhid)
         self.lin2 = torch.nn.Linear(self.nhid, self.nhid//2)
@@ -31,7 +32,7 @@ class Net(torch.nn.Module):
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
-
+        
         x = F.relu(self.conv1(x, edge_index))
         x, edge_index, _, batch,_ ,_ = self.pool1(x, edge_index, None, batch)
         x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
@@ -53,4 +54,78 @@ class Net(torch.nn.Module):
 
         return x
 
+class TopKNet(SAGNet):
+    def __init__(self, args):
+        super(TopKNet, self).__init__(args)
+        self.pool1 = TopKPooling(self.nhid, ratio=self.pooling_ratio)
+        self.pool2 = TopKPooling(self.nhid, ratio=self.pooling_ratio)
+        self.pool3 = TopKPooling(self.nhid, ratio=self.pooling_ratio)
     
+class EdgeNet(SAGNet):
+    def __init__(self, args):
+        super(EdgeNet, self).__init__(args)
+        self.pool1 = EdgePooling(self.nhid)
+        self.pool2 = EdgePooling(self.nhid)
+        self.pool3 = EdgePooling(self.nhid)
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        
+        x = F.relu(self.conv1(x, edge_index))
+        x, edge_index, batch, _ = self.pool1(x, edge_index, batch)
+        x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv2(x, edge_index))
+        x, edge_index, batch, _ = self.pool2(x, edge_index, batch)
+        x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv3(x, edge_index))
+        x, edge_index, batch, _ = self.pool3(x, edge_index, batch)
+        x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = x1 + x2 + x3 
+
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=self.dropout_ratio, training=self.training)
+        x = F.relu(self.lin2(x))
+        x = F.log_softmax(self.lin3(x), dim=-1)
+
+        return x
+    
+class ClusterNet(EdgeNet):
+    def __init__(self, args):
+        super(ClusterNet, self).__init__(args)
+        self.pool1 = ClusterPooling(self.nhid)
+        self.pool2 = ClusterPooling(self.nhid)
+        self.pool3 = ClusterPooling(self.nhid)
+
+class ASANet(SAGNet):
+    def __init__(self, args):
+        super(ASANet, self).__init__(args)
+        self.pool1 = ASAPooling(self.nhid, self.pooling_ratio)
+        self.pool2 = ASAPooling(self.nhid, self.pooling_ratio)
+        self.pool3 = ASAPooling(self.nhid, self.pooling_ratio)
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        
+        x = F.relu(self.conv1(x, edge_index))
+        x, edge_index, _, batch,_ = self.pool1(x, edge_index, None, batch)
+        x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv2(x, edge_index))
+        x, edge_index, _, batch,_ = self.pool2(x, edge_index, None, batch)
+        x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv3(x, edge_index))
+        x, edge_index, _, batch,_ = self.pool3(x, edge_index, None, batch)
+        x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = x1 + x2 + x3 
+
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=self.dropout_ratio, training=self.training)
+        x = F.relu(self.lin2(x))
+        x = F.log_softmax(self.lin3(x), dim=-1)
+
+        return x
